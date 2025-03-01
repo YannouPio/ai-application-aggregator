94918d6074d0064fd65b037a0fe4db3fa93dada4
PENG JINGQIANG <91521556+YannouPio@users.noreply.github.com>
2025-03-01 19:00:40 +0800

    Enhance dashboard UI with search functionality in Header and improve SideNav layout

--- src/app/dashboard/_components/Header.tsx
+++ src/app/dashboard/_components/Header.tsx
@@ -1,8 +1,17 @@
+import { Search } from 'lucide-react'
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='p-5 shadow-sm border-b-2 bg-white flex justify-between items-center'>
+      <div className='flex gap-2 items-center
+       p-2 border rounded-md max-w-lg bg-white'>
+        <Search />
+        <input type='text' placeholder='Search...'
+          className='outline-none'
+        />
+      </div>
+    </div>
   )
 }
 
--- src/app/dashboard/_components/SideNav.tsx
+++ src/app/dashboard/_components/SideNav.tsx
@@ -10,19 +10,18 @@ function SideNav() {
         {name: 'Billing', icon: Home, link: '/dashboard/billing'},
         {name: 'Settings', icon: Settings, link: '/dashboard/settings'},
     ]
-
-
     return (
         <div className='h-screen p-5 shadow-sm border'>
-            <div className='flex justify-center border-b'>
+            <div className='flex justify-center'>
             <Image src={'/logo.svg'} alt='logo' width={100} height={100} />
             </div>
-            <div className='mt-10'>
+            <hr className='my-6 border' />
+            <div className='mt-3'>
                 {MenuList.map((menu, index) => (
                     <div className='flex gap-2 mb-2 p-3 hover:bg-gray-600 hover:text-white rounded-lg
                     cursor-pointer' key={index}>
-                        <menu.icon />
-                        <h2>{menu.name}</h2>
+                        <menu.icon className='h-6 w-6' />
+                        <h2 className='text-lg'>{menu.name}</h2>
                     </div>
                 ))}
             </div>
