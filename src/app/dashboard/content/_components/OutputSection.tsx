429471d6a4e5b091911fd02698d745a1d42e4c5a
PENG JINGQIANG <91521556+YannouPio@users.noreply.github.com>
2025-03-04 18:41:34 +0800

    Add Toast UI Editor to OutputSection for enhanced content editing

--- src/app/dashboard/content/_components/OutputSection.tsx
+++ src/app/dashboard/content/_components/OutputSection.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { Editor } from '@toast-ui/react-editor';
+import { Button } from '@/components/ui/button';
+import { Copy } from 'lucide-react';
 
 function OutputSection() {
+  const editorRef:any = useRef(null);
   return (
-    <div>OutputSection</div>
+    <div className=' bg-white shadow-lg border rounded-lg'>
+      <div className='flex justify-between items-center p-5'>
+        <h2 className='font-medium text-lg'>你的结果</h2>
+        <Button className='flex gap-2'><Copy />Copy</Button>
+      </div>
+      <Editor
+        ref={editorRef}
+        initialValue="输出结果在此"
+        initialEditType="wysiwyg"
+        height="600px"
+        useCommandShortcut={true}
+        onChange={()=>console.log(editorRef.current.getInstance().getMarkdown())}
+      />
+    </div>
   )
 }
 
