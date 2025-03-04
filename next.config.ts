752042b4f33f9dad0307b1fb159afdaecbfb0316
PENG JINGQIANG <91521556+YannouPio@users.noreply.github.com>
2025-03-04 16:44:52 +0800

    Refactor image domain configuration in next.config.ts to use remotePatterns and update CreateNewContent function to handle async props

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['cdn-icons-png.flaticon.com', 'github.com', 'raw.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn-icons-png.flaticon.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+    ],
   }
 };
 
--- src/app/dashboard/content/[template-slug]/page.tsx
+++ src/app/dashboard/content/[template-slug]/page.tsx
@@ -10,9 +10,10 @@ interface PROPS{
   }
 }
 
-function CreateNewContent(props: PROPS) {
-  const selectedTemplate:TEMPLATE|undefined=Templates?.find((item)=>item.slug==props.params['template-slug']);
- 
+async function CreateNewContent(props: PROPS) {
+  const { 'template-slug': templateSlug } = await props.params;
+  const selectedTemplate:TEMPLATE|undefined=Templates?.find((item)=>item.slug==templateSlug);
+  
   return (
     <div className='grid grid-cols-1 md:grid-cols-3 gap-5 py-5' >
       {/* FormSection */}
